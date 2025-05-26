81906a2a78bd4b327ce22ec8a8dcb96ef3ab3f44
Abhishek Gadhar <abhishekgadhar2002@gmail.com>
2025-05-26 11:10:16 +0530

    Add getUserChannelProfile function to retrieve user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -350,6 +350,85 @@ const updateUserCover= asyncHandler(async (req ,res )=>{
 })
 
 
+
+
+
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+ const {username} = req.params
+
+ if(!username?.trim()){
+  throw new ApiError(400,"Username is missing")
+ }
+
+ const channel = await User.aggregate([
+   {
+       $match: {
+        username: username?.toLowerCase()
+       }
+   },
+   {
+    $lookup:{
+      from: "Subscription",
+      localField: "_id",
+      foreignField : "channel",
+       as: "subscribers"
+    }
+  },
+    {
+    $lookup:{
+      from: "Subscription",
+      localField: "_id",
+      foreignField : "subscriber",
+       as: "subscribedTo"
+    }
+   },
+   {
+    $addFields: {
+      subscribersCount : {
+        $size: "$subscribers"
+      },
+      channelsSubscribedToCount: {
+        $size : "$subscribedTo"
+      },
+      isSubscribed: {
+        $cond: {
+          if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+          then: true,
+          else: false
+        }
+      }
+    }
+   },
+   {
+    $project: {
+      fullname: 1,
+      username: 1,
+      subscribersCount: 1,
+      channelsSubscribedToCount: 1,
+      isSubscribed: 1,
+      avatar: 1,
+      coverImage: 1,
+      email: 1
+    }
+   }
+
+
+ ])
+
+console.log("channel:", channel);
+
+if(!channel?.length){
+  throw new ApiError(404 , "Channel doesnot exist");
+}
+
+return res
+.status(200)
+.json(
+  new ApiResponse(200 , channel[0], "User Channel Fetched Successfully")
+)
+})
+
+
 // **Default export**
 export { registerUser,
          loginUser,
@@ -359,7 +438,8 @@ export { registerUser,
          getCurrentUser,
          updateAccountDetails,
          updateUserAvatar,
-         updateUserCover
+         updateUserCover,
+         getUserChannelProfile
          
 
  };
